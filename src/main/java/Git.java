9a2936f3d72c5c4bd0e1992f6c79e80534a1d712
diego <diego@192.168.15.7>
2021-06-22 03:02:51 -0300

    Integrando Git com Netbeans

---
+++ src/main/java/Git.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author diego
+ */
+public class Git {
+    
+    public static void main(String[] args){
+        System.out.println("Projeto com Git");
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
